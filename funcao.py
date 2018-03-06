543958f87904551a2be186018ef625bbd499ae5c
Alexandre <logan@localhost.localdomain>
2018-03-06 16:16:26 -0300

    arquivos adicionados

--- funcao.py
+++ funcao.py
@@ -1,8 +1,16 @@
-def soma(x=3, y=5):
-    print("x: " + str(x))
-    print("y: " + str(y))
-    return x + y
+# def soma(x=3, y=5):
+#     print("x: " + str(x))
+#     print("y: " + str(y))
+#     return x + y
 
-s =soma(y=10, x=13)
+# s =soma(y=10, x=13)
 
-print(s)
+# print(s)
+
+a = 1
+b = 2
+
+a = a, b
+
+print(a[0])
+# print(b)
