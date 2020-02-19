9b63b75d25567c44c29549a6bd651c3f804b070e
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-02-19 18:30:51 -0300

    dicts file created

---
+++ 106.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+## DICTIONARIES (dicts)
+
+# Os dicts são vetores associativos, tambem conhecidos como maps
+
+# Dicionarios sao semelhantes as listas:
+# - ambos podem conter tipos de dados variados
+# - ambos sao iteraveis
+# - ambos sao mutaveis
+
+# Dicts sao diferentes das listas:
+# - dicionarios nao sao ordenados. A ordem nao eh preservada e nao eh importante para essa estrutura de dados
+# - o tempo de busca no dicionarioe constante (O(1)), independentemente do tamanho do dicionario
+
+# Dicts são como dicionarios reais:
+# - os dicts são feitos de pares 'chave':valor
+# - as chaves do dicts devem ser unicas
+# - a chave serve para procurar o valor, mas não o contrário
