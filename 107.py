7890ecb697bd8aa16feb4bb23209785a016f7bd1
alegume <alexandreabreu@comp.ufla.br>
2020-02-20 17:11:21 -0300

    just a draft

---
+++ 107.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+## Tuples
+
+
+
+
+
+
+
+
+###
+# Exercicio
+###
+
+# 1) Faca uma funcao para ler o arquivo lista-cpf.txt e gere um arquivo lista-cpf-unicos.txt que ignore os cpfs duplicados
