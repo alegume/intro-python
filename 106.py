d6c75f7ec2efd38822fc589042ccfb3a99160042
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-02-26 21:58:26 -0300

    challenges finished

--- 106.py
+++ 106.py
@@ -109,3 +109,4 @@ print(courses)
 
 # 1) Implemente o metodo define_default_city de acordo com a docstring definida no inicio da funcao. Utilize a clausula else no loop implementado.
 # 2) Remova do arquivo capitais-BR.csv todas capitais dos estados do sudeste e teste se sua funcao estah robusta o suficiente.
+# 3) Faca uma funcao que le o arquivo lista-cpf.txt, retorne a quantidade de CPF unicos (sem repeticao) e os escreva em um arquivo lista-cpf-unicos.txt. Eh necessario descompactar o arquivo lista-cpf.txt.tar.gz primeiro.
