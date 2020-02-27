7a61c91b53e84bb550a588f9345f108c9899cda8
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-02-26 22:21:22 -0300

    object scratched

--- 107.py
+++ 107.py
@@ -10,6 +10,47 @@
 
 ## Objetos
 
+class Animal:
+	tipo = "Mamifero"
+
+	def getTipo(self):
+		return self.tipo
+
+
+class Catiorro(Animal):
+	raca = "vira-lata"
+
+	def __init__(self, nome="", raca="", idade=None):
+		self.nome = nome
+		self.idade = idade
+		if raca == '':
+			self.raca = "vira lata"
+		else:
+			self.raca = raca
+
+	def __str__(self):
+		return '{} - {} ({})'.format(self.nome, self.raca, self.idade)
+
+	# # sobrescrita
+	# def getTipo(self):
+	# 	return "Catiorro"
+
+
+doguinho = Catiorro("Zeca", idade=3)
+
+print(type(doguinho))
+# print(doguinho.nome)
+print(doguinho)
+
+# Eh possivel atribuir valor a um atributo ainda nao definido
+doguinho.dono = "Maria"
+print('O dono eh a {}'.format(doguinho.dono))
+
+# Heranca
+print("{1} eh do tipo {0}".format(doguinho.getTipo(), doguinho.nome))
+
+
+
 
 
 
