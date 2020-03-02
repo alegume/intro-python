9a7181324d9e97d43267021aa741d8e54c101e8d
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-03-02 17:04:47 -0300

    objects and classes finished

--- 108.py
+++ 108.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 
-## Classes e Objetos
-# metodos magicos: https://www.python-course.eu/python3_magic_methods.php
+from datetime import date
 
+## Classes e Objetos
+
+# Primeira classe
 class Animal:
+	# atributo comum
 	tipo = "Mamifero"
 
-	def getTipo(self):
+	# metodo da classe
+	def get_tipo(self):
 		return self.tipo
 
 
+# Segunda classe
 class Catiorro(Animal):
-	raca = "vira-lata"
+	# Atributo protegido (protected)
+	_data_criacao_ = date.today()
 
 	# dunder init dunder eh o construtor da classe. Dunder = double underscore
 	# self eh uma referencia ao objeto. Pode ser usado outro nome, mas nao eh comum.
@@ -23,14 +29,23 @@ class Catiorro(Animal):
 		else:
 			self.raca = raca
 
+	# outro metodo privado e 'magico'
+	# metodos magicos: https://www.python-course.eu/python3_magic_methods.php
 	def __str__(self):
 		return '{} - {} ({})'.format(self.nome, self.raca, self.idade)
 
+	def imprimir_criacao(self):
+		# formatacao da data com strftime
+		print(self._data_criacao_.strftime('%d/%m/%Y'))
+
 	# # sobrescrita
-	# def getTipo(self):
+	# def get_tipo(self):
 	# 	return "Catiorro"
 
 
+## Utilizacao das classes
+
+# Instancializacao de objeto
 doguinho = Catiorro("Zeca", idade=3)
 
 print(type(doguinho))
@@ -42,13 +57,8 @@ doguinho.dono = "Maria"
 print('O dono eh a {}'.format(doguinho.dono))
 
 # Heranca
-print("{1} eh do tipo {0}".format(doguinho.getTipo(), doguinho.nome))
+print("{1} eh do tipo {0}".format(doguinho.get_tipo(), doguinho.nome))
 
-
-
-
-
-
-###
-# Exercicio
-###
+# Instancializacao de outro objeto
+dog1 = Catiorro()
+doguinho.imprimir_criacao()
