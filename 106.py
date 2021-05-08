98b43d7ce7ffda812077f98d4389a1b8add869c7
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2021-05-08 16:44:43 -0300

    106 updated

--- 106.py
+++ 106.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 ## DICTIONARIES (dicts)
 
 # Os dicts são vetores associativos, tambem conhecidos como maps
@@ -21,12 +23,12 @@
 # Apenas uma funcao vazia. Voce ira implementa-la futuramente :p
 # LER PEP 257
 def define_default_city(state):
-	''' Define a capital do estado de origem como city_origin para um professor existente no arquivo. Retorna True se a capital do estado de origem existe no arquivo capitais-BR.csv e False, caso contrario.
+    ''' Define a capital do estado de origem como city_origin para um professor existente no arquivo. Retorna True se a capital do estado de origem existe no arquivo capitais-BR.csv e False, caso contrario.
 
-	Keyword arguments:
-		state -- O estado de origem do professor
-	'''
-	pass
+    Keyword arguments:
+        state -- O estado de origem do professor
+    '''
+    pass
 
 ## Exemplos
 
@@ -40,6 +42,8 @@ print(len(professor1))
 print(professor1.keys())
 # Valores
 print(professor1.values())
+
+sys.exit(0)
 
 professor2 = {'id': 37, 'name': 'Denilson Barbosa', 'age': 40, 'state_origin': 'Paraná', 'courses': ['Inteligência Artificial', 'Banco de Dados I', 'Banco de Dados II', 'Programação para Internet I']}
 
@@ -67,15 +71,15 @@ print(professor1['state_origin'])
 
 # 1) Simples e direta
 if 'city_origin' in professor1:
-	print(professor1['city_origin'])
+    print(professor1['city_origin'])
 else:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
+    print(f'{professor1["name"]} Ainda não possui uma cidade')
 
 # 2) Tratar a excecao
 try:
-	print(professor1['city_origin'])
+    print(professor1['city_origin'])
 except KeyError:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
+    print(f'{professor1["name"]} Ainda não possui uma cidade')
 
 
 # 3) Usando o metodo get  8-)
@@ -83,18 +87,18 @@ except KeyError:
 # city = professor1.get('city_origin', None)
 city = professor1.get('city_origin', 'Canoinhas')
 if city == None:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
+    print(f'{professor1["name"]} Ainda não possui uma cidade')
 else:
-	print(city)
+    print(city)
 
 # Percorrer os elementos pela chaves
 for key in professor3.keys():
-	value = professor3[key]
-	print(key, ':', value)
+    value = professor3[key]
+    print(key, ':', value)
 
 # Percorrer os elementos pela chaves e valores
 for key, value in professor2.items():
-	print(key, ':', value)
+    print(key, ':', value)
 
 # Remove chave:valor do dict usando pop(chave)
 courses = professor3.pop('courses')
@@ -108,5 +112,5 @@ print(courses)
 
 
 # 1) Implemente o metodo define_default_city de acordo com a docstring definida no inicio da funcao. Utilize a clausula else no loop implementado.
-# 2) Remova do arquivo capitais-BR.csv todas capitais dos estados do sudeste e teste se sua funcao estah robusta o suficiente.
-# 3) Faca uma funcao que le o arquivo lista-cpf.txt, retorne a quantidade de CPF unicos (sem repeticao) e os escreva em um arquivo lista-cpf-unicos.txt. Eh necessario descompactar o arquivo lista-cpf.txt.tar.gz primeiro.
+# 2) Remova do arquivo capitais-BR.csv todas capitais dos estados do sudeste e teste se sua funcao estah robusta o suficiente. Ela deve executar sem erro mesmo que alguns dados estejam faltando.
+# 3) Faca uma funcao que le o arquivo lista-cpf.txt, retorne a quantidade de CPF unicos (sem repeticao) e os escreva em um arquivo lista-cpf-unicos.txt. Eh necessario descompactar o arquivo lista-cpf.txt.tar.gz primeiro. O algoritmo precisa ser eficiente, nesse caso especifico a melhor a melhor complexidade que pode ser acancada é linear. Algoritmos de complexidade quadratica, cubica, fatorial, etc. não sao considerados como eficientes pois a complexidade linear eh garantida. Como regra geral, se seu algoritmo demorar mais do que alguns segundos, ele, provavelmente, nao eh linear.
